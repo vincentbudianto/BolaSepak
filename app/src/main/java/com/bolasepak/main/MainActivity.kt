3d784862bf43a210732982dfc0177f8122472eb8
vincentbudianto <vincent.budianto@gmail.com>
2020-03-12 17:58:52 +0700

    Fix step counter

--- app/src/main/java/com/bolasepak/main/MainActivity.kt
+++ app/src/main/java/com/bolasepak/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bolasepak.main
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -9,15 +10,17 @@ import android.view.View
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.bolasepak.api.TheSportDBApi
 import com.bolasepak.event.EventFragment
+import com.bolasepak.listener.StepListener
+import com.bolasepak.util.StepDetector
 import com.bolasepak.R
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity(), SensorEventListener, StepListener {
     private var content: FrameLayout? = null
+    private var stepDetector: StepDetector? = null
+    private var steps: Int = 0
     var sensorManager: SensorManager? = null
-    var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
 
     private fun addFragment(fragment: EventFragment) {
         supportFragmentManager
@@ -30,6 +33,10 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        stepDetector = StepDetector()
+        stepDetector!!.registerListener(this)
+
         val fragment = EventFragment.newInstance("eventspastleague")
         addFragment(fragment)
     }
@@ -37,12 +44,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onResume() {
         super.onResume()
 
-        if (stepsSensor == null) {
+        var accelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        if (accelerometer == null) {
             Toast.makeText(this, "Step Counter Not Supported", Toast.LENGTH_SHORT).show()
             stepCounter.visibility = View.INVISIBLE
             stepComment.visibility = View.INVISIBLE
         } else {
-            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+            sensorManager!!.registerListener(this, sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST)
         }
     }
 
@@ -50,8 +59,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        if (stepsSensor != null) {
-            stepCounter.text = "" + event.values[0]
+        if (event!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            stepDetector!!.updateAccelerometer(event.timestamp, event.values[0], event.values[1], event.values[2])
         }
     }
+
+    override fun step(timeNs: Long) {
+        steps++
+        stepCounter.text = steps.toString()
+    }
+
 }
